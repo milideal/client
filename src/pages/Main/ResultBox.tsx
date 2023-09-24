e79adcb5310a619c87714e2c97fd8da3cd23ce1b
i4song <i4song.dev@gmail.com>
2023-09-24 19:45:25 +0900

    :crescent_moon: [Done\!] Add dark mode style

--- src/pages/Main/ResultBox.tsx
+++ src/pages/Main/ResultBox.tsx
@@ -7,7 +7,7 @@ const ResultBox = (props: {
   return props.result.map((r) => (
     <Link key={r.id} to={`/map?x=${r.x}&y=${r.y}`}>
       <div className="vbox ml(12) mt(20) w(358) pointer">
-        <span className="font(14) NanumGothic 900 c(#363636) dark:c(#fff)">
+        <span className="font(14) NanumGothic 900 c(#363636) dark-white">
           {r.place_name}
         </span>
         <span className="font(14) NanumGothic 900 c(#999999)">
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -31,7 +31,7 @@ const Menu = () => {
   return (
     <div className="vbox pack ml(30) mr(30)">
       <div className="vbox w(370)">
-        <span className="font(32) GmarketSans 900 c(#363636) dark:c(#fff)">
+        <span className="font(32) GmarketSans 900 c(#363636) dark-white">
           총 1,214개의
           <br /> 군인 할인점들이 있어요.
         </span>
@@ -40,7 +40,7 @@ const Menu = () => {
         </span>
       </div>
       <div className="wrap w(370) mt(50) mb(50)">
-        <span className="font(16) GmarketSans 900 c(#363636) dark:c(#fff) mb(10)">
+        <span className="font(16) GmarketSans 900 c(#363636) dark-white mb(10)">
           어디서 찾아볼까요?
         </span>
         <div className="w(370) h(40) b(2/#505050) r(20) pl(12) hbox">
@@ -54,14 +54,15 @@ const Menu = () => {
             onChange={onChange}
             onKeyDown={detectEnter}
             value={keyword}
-            className="placeholder::font(14)+900+c(#999999)+NanumGothic NanumGothic font(14) 900 c(#363636) dark:c(#fff) w(300) dark:bg(#000.0)"
+            spellCheck={false}
+            className="placeholder::font(14)+900+c(#999999)+NanumGothic NanumGothic font(14) 900 c(#363636) dark-white w(300) dark-transparent"
             placeholder="강남구 삼성동"
           />
         </div>
         <ResultBox result={searchResults} />
       </div>
       <div className="vbox w(370) r(5) b(1/#D9D9D9) box-shadow(0/0/20/#000.1) pl(24) pt(14) pb(14)">
-        <span className="font(14) 900 c(#363636) dark:c(#fff)">
+        <span className="font(14) 900 c(#363636) dark-white">
           할인점 추가하기
         </span>
         <span className="font(14) 900 c(#999999)">

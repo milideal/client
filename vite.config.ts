0492e90c9b11f7d51f0aaadb523e2e83919a790a
Yongwook Lee <i4song.dev@gmail.com>
2023-09-24 11:35:46 +0900

    :closed_lock_with_key: Make Kakao app key into environment variable (#4)
    * :closed_lock_with_key: Make Kakao app key into environment variable
    
    * :memo: Add `.env.example`
    
    ---------
    
    Co-authored-by: junglesub <junglesubmarine@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { adorableCSS } from "adorable-css/vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [adorableCSS(), react()],
-  server: {
-    host: true,
-    port: 3000
-  }
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [
+      adorableCSS(),
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            kakaoAppKey: env.VITE_KAKAO_MAP_APP_KEY,
+          },
+        },
+      }),
+    ],
+    server: {
+      host: true,
+      port: 3000,
+    },
+  });
+};

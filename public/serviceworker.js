8173afcb3ed5b0509ae1fa5c963505bef151b763
i4song <i4song.dev@gmail.com>
2023-09-24 18:27:42 +0900

    :bug: [Retry] Fix unsupported MIME type error on deployed page

---
+++ public/serviceworker.js
@@ -0,0 +1,9 @@
+// install event
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+self.addEventListener("install", (e) => {});
+
+// activate event
+self.addEventListener("activate", (e) => {});
+
+// fetch event
+self.addEventListener("fetch", (e) => {});

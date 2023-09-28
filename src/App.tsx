1e05fd63a6ff966a872c59182aefc8c08dca5980
junglesub <junglesubmarine@gmail.com>
2023-09-28 03:23:12 +0000

    :zap: Move MapView to own directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Menu from "./pages/Menu";
-import MapView from './pages/MapView';
+import MapView from "./pages/MapView/MapView";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element = {<Menu />} />
-      <Route path="/map" element = {<MapView />} />
+      <Route path="/" element={<Menu />} />
+      <Route path="/map" element={<MapView />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;

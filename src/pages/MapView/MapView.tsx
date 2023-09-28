61f798169781a5980afccc4818f6b5b0b797e0f2
junglesub <junglesubmarine@gmail.com>
2023-09-28 04:32:25 +0000

    :sparkles: add map info modal

--- src/pages/MapView/MapView.tsx
+++ src/pages/MapView/MapView.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import ReactDomServer from "react-dom/server";
+import MapViewModal from "./MapViewModal";
 
 const MapView = () => {
   useEffect(() => {
@@ -39,6 +41,24 @@ const MapView = () => {
         image: markerImage, // 마커 이미지
       });
       console.log(marker);
+
+      // 마커 클릭 이벤트
+      kakao.maps.event.addListener(
+        map,
+        "click",
+        function (mouseEvent: kakao.maps.event.MouseEvent) {}
+      );
+
+      const iwContent = <MapViewModal />;
+      const iwPosition = new kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
+        iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+      // 인포윈도우를 생성하고 지도에 표시합니다
+      const infowindow = new kakao.maps.InfoWindow({
+        map: map, // 인포윈도우가 표시될 지도
+        position: positions[0].latlng,
+        content: ReactDomServer.renderToStaticMarkup(iwContent),
+        removable: iwRemoveable,
+      });
     }
   }, []);
 
---
+++ src/pages/MapView/MapViewModal.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import locationImage from "../../assets/MapView/location.svg";
+import phoneIcon from "../../assets/MapView/phone.svg";
+import websiteIcon from "../../assets/MapView/website.svg";
+import descriptionIcon from "../../assets/MapView/description.svg";
+
+function MapViewModal() {
+  return (
+    <div className="MapViewModal w(341) m(29) r(18)">
+      <div
+        className="heading text(center) bold"
+        style={{ marginBottom: "32px" }}
+      >
+        <div className="c(#999) font-size(14)">숙박시설</div>
+        <div className="font-size(24)">서귀포호텔</div>
+      </div>
+
+      <div className="info vbox gap(6)">
+        <div className="hbox gap(12)">
+          <div className="icon">
+            <img src={locationImage} />
+          </div>
+          <div className="text">제주도 서귀포시 상예로 319</div>
+        </div>
+        <div className="phone hbox gap(12)">
+          <div className="icon">
+            <img src={phoneIcon} />
+          </div>
+          <div className="text">064-738-0123</div>
+        </div>
+        <div className="desc hbox(top) gap(12)">
+          <div className="icon">
+            <img src={descriptionIcon} />
+          </div>
+          <div className="text" style={{ flexShrink: 1 }}>
+            해당 시설은 국군복지단에서 운영하는 군 휴양시설로, 자세한 내용은
+            국군복지단 홈페이지를 통해 확인하시기 바랍니다.
+          </div>
+        </div>
+        <div className="site desc hbox gap(12)">
+          <div className="icon">
+            <img src={websiteIcon} />
+          </div>
+          <div className="text">www.welfare.mil.kr</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MapViewModal;

2146ed603be18d256af61e5fba929fbdf8cb0af2
i4song <i4song.dev@gmail.com>
2023-09-29 18:38:13 +0900

    :lipstick: Minor style changes

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -40,12 +40,12 @@ const Menu = () => {
           총 1,214개의
           <br /> 군인 할인점들이 있어요.
         </span>
-        <span className="font(16) GmarketSans 900 c(#999999)">
+        <span className="font(16) NanumGothic 900 c(#999999)">
           23.09.19 업데이트
         </span>
       </div>
       <div className="wrap w(370) mt(50) mb(50)">
-        <span className="font(16) GmarketSans 900 c(#363636) dark-white mb(10)">
+        <span className="font(16) NanumGothic 900 c(#363636) dark-white mb(10)">
           어디서 찾아볼까요?
         </span>
         <div className="w(370) h(40) b(2/#505050) r(20) pl(12) hbox">
@@ -60,17 +60,17 @@ const Menu = () => {
             onKeyDown={detectEnter}
             value={keyword}
             spellCheck={false}
-            className="placeholder::font(14)+900+c(#999999)+NanumGothic NanumGothic font(14) 900 c(#363636) dark-white w(300) dark-transparent"
-            placeholder="강남구 삼성동"
+            className="placeholder::font(14)+900+c(#aaa) NanumGothic font(14) 900 c(#363636) dark-white w(300) dark-transparent"
+            placeholder="양구시외버스터미널"
           />
         </div>
         {isSearchCompleted && <ResultBox result={searchResults} />}
       </div>
       <div className="vbox w(370) r(5) b(1/#D9D9D9) box-shadow(0/0/20/#000.1) pl(24) pt(14) pb(14)">
-        <span className="font(14) 900 c(#363636) dark-white">
+        <span className="font(14) 600 c(#363636) NanumGothic dark-white">
           할인점 추가하기
         </span>
-        <span className="font(14) 900 c(#999999)">
+        <span className="font(14) 600 c(#999999) NanumGothic ">
           군인 장병들에게 잊지 못할 경험을 선물하세요.
         </span>
       </div>
